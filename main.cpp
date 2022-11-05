d7552d6702588c8577772c35b43e72c19d10eaa8
Noanono <soler.noah@gmail.com>
2022-11-05 19:47:22 +0100

    sepration du code en fichiers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 
+#include "myvars.h"
+
 using namespace std;
 
 int main() {
@@ -8,21 +10,12 @@ int main() {
     cout << "Jeu du pendu" << endl;
 
     //Déclaration des variables
-    //Liste de mots du jeu du pendu
-    vector<string> mots = {"abaisser", "abandon", "abasourdir", "abattre", "abdiquer", "abeille", "abolir", "aborder", "aboutir", "aboyer", "abrasif", "abreuver", "abriter", "abroger", "abrupt", "absence", "absolu", "absurde", "abusif", "abuter", "académie", "acajou", "acarien", "accabler", "accepter", "acclamer", "accolade", "accroche", "accuser", "acerbe", "achat", "acheter", "aciduler", "acier", "acompte", "acquérir", "acronyme", "acteur", "actif", "actuel", "adepte", "adequat", "adhésif", "adjectif", "adjuger", "admettre", "admirer", "adopter", "adorer", "adoucir", "adresse", "adroit", "adulte", "adverbe", "aérer", "aéroport", "affaire", "affecter", "affiche", "affreux", "affubler", "agacer", "agencer", "agile", "agiter", "agrafer", "agréable", "agrume", "aider", "aiguille", "ailier", "aimable", "aisance", "ajouter", "ajuster", "alarmer", "alchimie", "alerte", "algèbre", "algue", "aliéner", "aliment", "alléger", "alliage", "allouer", "allumer", "alourdir", "alpaga", "altesse", "alvéole", "amateur", "ambigu", "ambre", "aménager", "amertume", "amidon", "amiral", "amorcer", "amour", "amovible", "amphibie", "ampleur", "amusant", "analyse", "anaphore", "anarchie", "anatomie", "ancêtre", "anéantir", "angle", "angoisse", "anguleux", "animal", "annexer",
-    "annonce", "annuel", "anodin", "anomalie", "anonyme", "anormal", "antenne", "antidote", "anxieux", "apaiser", "apéritif", "aplanir", "apologie", "appareil", "appeler", "apporter", "appuyer", "aquarium", "aqueduc", "arbitre", "arbuste", "ardeur", "ardoise", "argent", "arlequin", "armature", "armoire", "armure", "arpenter", "arracher", "arriver", "arroser", "arsenic", "artefact", "article", "aspect", "asphalte", "aspirer", "assaut", "asservir", "assiette", "associer", "assurer", "asticot", "astre", "astuce", "atelier", "atome", "atrium", "atroce", "attaque", "attentif", "attirer", "attraper", "aubaine", "auberge", "audace", "audible", "augurer", "aurore", "automne", "autruche", "avaler", "avancer", "avarice", "avenir", "averse", "aveugle", "aviateur", "avide", "avion", "aviser", "avoine", "avouer", "avril", "axiome", "badge", "bagage", "baguette", "baignade", "balancer", "balcon", "baleine", "balisage", "bambin", "bancaire", "bandage", "banlieue", "bannière", "banquier", "barbier", "baril", "baron", "barque", "barrage", "bassin", "bastion", "bataille", "bateau", "batterie", "baudrier", "bavard", "belette", "bélier", "belote", "bénéfice", "berceau", "berger", "berline", "bermuda", "besace", "besogne", "bétail", "beurre",
-    "biberon", "bicycle", "bidule", "bijou", "bilan", "bilingue", "billard", "binaire", "biologie", "biopsie", "biotype", "biscuit", "bison", "bistouri", "bitume", "bizarre", "blafard", "blague", "blanchir", "blessant", "blinder", "blond", "bloquer", "blouson", "bobard", "bobine", "boire", "boiser", "bolide", "bonbon", "bondir", "bonheur", "bonifier", "bonus", "bordure", "borne", "botte", "boucle", "boueux", "bougie", "boulon", "bouquin", "bourse", "boussole", "boutique", "boxeur", "branche", "brasier", "brave", "brebis", "brèche", "breuvage", "bricoler", "brigade", "brillant", "brioche", "brique", "brochure", "broder", "bronzer", "brousse", "broyeur", "brume", "brusque", "brutal", "bruyant", "buffle", "buisson", "bulletin", "bureau", "burin", "bustier", "butiner", "butoir", "buvable", "buvette", "cabanon", "cabine", "cachette", "cadeau", "cadre", "caféine", "caillou", "caisson", "calculer", "calepin", "calibre", "calmer", "calomnie", "calvaire", "camarade", "caméra", "camion", "campagne", "canal", "caneton", "canon", "cantine", "canular", "capable", "caporal", "caprice", "capsule", "capter", "capuche", "carabine", "carbone", "caresser", "caribou", "carnage", "carotte", "carreau", "carton", "cascade"};
-    //Liste des etapes du dessin du pendu
-    vector<string> dessin_pendu = {"", " \n \n \n \n \n \n \n _______", " \n    |\n    |\n    |\n    |\n    |\n    |\n ___|___", " --|-/-----\n    |/\n    |\n    |\n    |\n    |\n    |\n ___|___", " --|-/--|--\n    |/   |\n    |\n    |\n    |\n    |\n    |\n ___|___", " --|-/--|--\n    |/   |\n    |    0\n    |\n    |\n    |\n    |\n ___|___", " --|-/--|--\n    |/   |\n    |    0\n    |    |\n    |    |\n    |\n    |\n ___|___", " --|-/--|--\n    |/   |\n    |    0\n    |  --|\n    |    |\n    |\n ___|___", " --|-/--|--\n    |/   |\n    |    0\n    |  --|--\n    |    |\n    |\n ___|___", " --|-/--|--\n    |/   |\n    |    0\n    |  --|--\n    |    |\n    |   /\n ___|___", " --|-/--|--\n    |/   |\n    |    0\n    |  --|--\n    |    |\n    |   / \\\n ___|___"};
     //Mot à trouver
     string mot;
     //Mot à trouver caché
     string motCache;
     //Lettre proposée
     string lettre;
-    //Nombre de coups
-    int nbEssais = 10;
-    bool trouve = false;
 
     //Choix du mot à trouver
     srand(time(NULL));
---
+++ myvars.h
@@ -0,0 +1,96 @@
+//
+// Created by soler on 05/11/2022.
+//
+
+#ifndef JEU_PENDU_CPP_MYVARS_H
+#define JEU_PENDU_CPP_MYVARS_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Liste des mots du jeu du pendu (sans majuscule ni accent)
+vector<string> mots = {"banane", "ordinateur", "terme", "programme", "variable", "tableau", "methode", "classe", "objet", "heritage", "polymorphisme", "abstraction", "encapsulation", "portee", "pointeur", "instance", "constructeur", "destructeur", "surcharge", "sous-classe", "super-classe", "heritage", "polymorphisme", "abstraction", "encapsulation", "portee", "pointeur", "instance", "constructeur", "destructeur", "surcharge", "sous-classe", "super-classe", "heritage", "polymorphisme", "abstraction", "encapsulation", "portee", "pointeur", "instance", "constructeur", "destructeur", "surcharge", "sous-classe", "super-classe", "heritage", "polymorphisme", "abstraction", "encapsulation", "portee", "pointeur", "instance", "constructeur", "destructeur", "surcharge", "sous-classe", "super-classe", "heritage", "polymorphisme", "abstraction", "encapsulation", "portee", "pointeur", "instance", "constructeur", "destructeur", "surcharge", "sous-classe", "super-classe", "heritage", "polymorphisme", "abstraction", "encapsulation", "portee", "pointeur", "instance", "constructeur", "destructeur", "surcharge", "sous-classe", "super-classe", "heritage", "polymorphisme", "abstraction", "encapsulation", "portee", "pointeur", "instance", "constructeur", "destructeur", "surcharge", "sous-classe", "super-classe"};
+//Liste des etapes du dessin du pendu
+vector<string> dessin_pendu = {"",
+                               " \n"
+                               " \n"
+                               " \n"
+                               " \n"
+                               " \n"
+                               " \n"
+                               " \n"
+                               " _______",
+                               " \n"
+                               "    |\n"
+                               "    |\n"
+                               "    |\n"
+                               "    |\n"
+                               "    |\n"
+                               "    |\n"
+                               " ___|___",
+                               "  --|-/-----\n "
+                               "    |/\n "
+                               "    |\n "
+                               "    |\n "
+                               "    |\n "
+                               "    |\n "
+                               "    |\n "
+                               " ___|___",
+                               "  --|-/--|--\n "
+                               "    |/   |\n "
+                               "    |\n "
+                               "    |\n "
+                               "    |\n "
+                               "    |\n "
+                               "    |\n "
+                               " ___|___",
+                               "  --|-/--|--\n "
+                               "    |/   |\n "
+                               "    |    0\n "
+                               "    |\n "
+                               "    |\n "
+                               "    |\n "
+                               "    |\n "
+                               " ___|___",
+                               "  --|-/--|--\n "
+                               "    |/   |\n "
+                               "    |    0\n "
+                               "    |    |\n "
+                               "    |    |\n "
+                               "    |\n "
+                               "    |\n "
+                               " ___|___",
+                               "  --|-/--|--\n "
+                               "    |/   |\n "
+                               "    |    0\n "
+                               "    |  --|\n "
+                               "    |    |\n "
+                               "    |\n "
+                               " ___|___",
+                               "  --|-/--|--\n "
+                               "    |/   |\n "
+                               "    |    0\n "
+                               "    |  --|--\n "
+                               "    |    |\n "
+                               "    |\n "
+                               " ___|___",
+                               "  --|-/--|--\n "
+                               "    |/   |\n "
+                               "    |    0\n "
+                               "    |  --|--\n "
+                               "    |    |\n "
+                               "    |   /\n "
+                               " ___|___",
+                               "  --|-/--|--\n "
+                               "    |/   |\n "
+                               "    |    0\n "
+                               "    |  --|--\n "
+                               "    |    |\n "
+                               "    |   / \\\n "
+                               " ___|___"};
+//Nombre de coups
+int nbEssais = 10;
+bool trouve = false;
+
+#endif //JEU_PENDU_CPP_MYVARS_H
